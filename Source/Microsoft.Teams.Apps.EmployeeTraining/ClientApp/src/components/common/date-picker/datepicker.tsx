f7a1662dc18a28a446f2ba77f87e9e46e7400dae
Mikhail Bondarevsky <mibon@microsoft.com>
2021-02-25 19:28:33 +0000

    Fix goToday is missing

--- Source/Microsoft.Teams.Apps.EmployeeTraining/ClientApp/src/components/common/date-picker/datepicker.tsx
+++ Source/Microsoft.Teams.Apps.EmployeeTraining/ClientApp/src/components/common/date-picker/datepicker.tsx
@@ -30,7 +30,8 @@ let dayPickerStrings: IDatePickerStrings = {
     months: ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"],
     shortMonths: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
     days: ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"],
-    shortDays: ["S", "M", "T", "W", "T", "F", "S"]
+    shortDays: ["S", "M", "T", "W", "T", "F", "S"],
+    goToToday: ""
 };
 
 const onFormatDate = (date?: Date): string => {
