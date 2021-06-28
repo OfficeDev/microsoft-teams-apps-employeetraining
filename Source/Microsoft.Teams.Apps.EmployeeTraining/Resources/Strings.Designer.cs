1bc86f06a6e906bc4d7cc81ae867dfd829620d2d
v-smahaj <60421876+v-smahaj@users.noreply.github.com>
2021-06-28 13:42:00 +0530

    Update Strings.Designer.cs

--- Source/Microsoft.Teams.Apps.EmployeeTraining/Resources/Strings.Designer.cs
+++ Source/Microsoft.Teams.Apps.EmployeeTraining/Resources/Strings.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Microsoft.Teams.Apps.EmployeeTraining.Resources {
+namespace Microsoft.Teams.Apps.EmployeeTraining {
     using System;
     
     
